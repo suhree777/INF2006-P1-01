5390db883d9173a3aeb425d40a8cb325a972a5a6
Xuanting85 <xuanting2000@gmail.com>
2024-02-17 12:11:03 +0800

    using html first, got board no pages

--- index.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <?php include "includes/head.inc.php"; ?>
-    <link rel="stylesheet" href="css/index.css">
-    <title>Home</title>
-</head>
-
-<body>
-    <?php include "includes/nav.inc.php" ?>
-</body>
